dd735527d6e0e51c3681156dae644dfc77133d92
lucy-mothership <lucy@wearemothership.com>
2023-12-27 14:11:04 +0000

    fix fold line

--- src/utils/fold-line.js
+++ src/utils/fold-line.js
@@ -2,13 +2,13 @@
 import runes from 'runes2'
 
 export default function foldLine(line) {
-  const parts = []
-  let length = 75
-  while (runes(line).length > length) {
-    parts.push(runes.substring(line, 0, length))
-    line = runes.substring(line, length)
-    length = 74
-  }
-  parts.push(line)
-  return parts.join('\r\n\t')
+//   const parts = []
+//   let length = 75
+//   while (runes(line).length > length) {
+//     parts.push(runes.substring(line, 0, length))
+//     line = runes.substring(line, length)
+//     length = 74
+//   }
+//   parts.push(line)
+  return line; //parts.join('\r\n\t')
 }
